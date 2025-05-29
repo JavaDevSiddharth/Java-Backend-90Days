118c29d857ec387706e249d4ccba0501b319a387
Siddharth jagdhane <jagdhnesiddharth@gmail.com>
2025-05-29 23:44:17 +0530

    String methods problems

--- spring_problems.java
+++ spring_problems.java
@@ -1,7 +1,7 @@
 public class spring_problems {
     public static void main(String[] args) {
         String name = " SIDDHARTH ";
-// using replace to spaces with underscore
+// using replacement to spaces with underscore
         String replace = name.replace(" ","_");
         System.out.println(replace);
 
@@ -9,14 +9,12 @@ public class spring_problems {
         String lower = name.toLowerCase();
         System.out.println(lower);
 
-//writing a  java code for fill in latters templete which looks like "Dear <|name |> thanks a lot !"
+//writing a  java code for fill-in latter template which looks like "Dear <|name |> thanks a lot !"
         System.out.println("Dear"+ name + "thanks a lot!");
 
-// write the program to format at the following latters using escape sequence  characters
+// write the program to format at the following latter using escape sequence  characters
         String letters = "Dear sid,\n\t this java is very nice and enjoyable.\n\t\t\t\t Thanks!";
         System.out.println(letters);
-
-
 
     }
 }
